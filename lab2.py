b6b3dcbe21453af687b23a0756fef00182d3d30b
Maliha Reynolds <pinkyanimegirl@gmail.com>
2025-07-23 14:37:04 -0700

    lab2

---
+++ lab2.py
@@ -0,0 +1,6 @@
+name="maliha reynolds"
+nickname="sunny" 
+age=17
+python=False
+hobbies=["reading","dance","tennis","volleyball"]
+favorite={"movie":"phantom of the opera","music":"straykids","food":"korean","hobby":"dance"}
